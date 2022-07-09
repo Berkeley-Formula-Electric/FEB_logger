326d28f892f4352fe7a0e04310b70817af09651d
-T.K.- <t_k_233@outlook.com>
2022-07-08 18:06:34 -0700

    ADD: add logger code from 2022 comp car

---
+++ FEB_logger.c
@@ -0,0 +1,15 @@
+/*
+ * FEB_logger.c
+ *
+ *  Created on: Jul 5, 2022
+ *      Author: TK
+ */
+#include "FEB_logger.h"
+
+extern UART_HandleTypeDef huart2;
+
+void FEB_log(char *module, char *level, char *msg) {
+  char str[256];
+  sprintf(str, "<%s> [%s] %s\r\n", module, level, msg);
+  HAL_UART_Transmit(&huart2, (uint8_t *)str, strlen(str), 100);
+}
---
+++ FEB_logger.h
@@ -0,0 +1,25 @@
+/*
+ * FEB_logger.h
+ *
+ *  Created on: Jul 5, 2022
+ *      Author: TK
+ */
+
+#ifndef FEB_CAN_FEB_LOGGER_H_
+#define FEB_CAN_FEB_LOGGER_H_
+
+#include <string.h>
+#include <stdio.h>
+
+#include "stm32f4xx_hal.h"
+
+/**
+ * @brief Log a message to debug serial output. This function will append a CRLF newline at the end of the message.
+ * 
+ * @param module the name of the module that is generating the log
+ * @param level level of the log message, can be DEBUG, INFO, WARNING, ERROR, or CRITICAL
+ * @param msg the text of the message
+ */
+void FEB_log(char *module, char *level, char *msg);
+
+#endif /* FEB_CAN_FEB_LOGGER_H_ */
